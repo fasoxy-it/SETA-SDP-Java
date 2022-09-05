a887ec920fbc61110cd90ae1154929dc63172d3d
Mattia Fasoli <fasoxy@gmail.com>
2022-09-05 12:47:15 +0200

    Create AdministratorServer

---
+++ src/main/java/REST/AdministratorServer.java
@@ -0,0 +1,21 @@
+package REST;
+
+import com.sun.jersey.api.container.httpserver.HttpServerFactory;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+
+public class AdministratorServer {
+
+    private static final String HOST = "localhost";
+    private static final int PORT = 1337;
+
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServerFactory.create("http://" + HOST + ":" + PORT + "/");
+        server.start();
+
+        System.out.println("Server running!");
+        System.out.println("Server started on: http://" + HOST + ":" + PORT + "/");
+    }
+
+}
