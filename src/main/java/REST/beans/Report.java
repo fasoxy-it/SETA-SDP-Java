9df8d7f4e342aa2c209c90bc1bf6013a325978df
Mattia Fasoli <fasoxy@gmail.com>
2022-09-08 11:51:45 +0200

    Update Report Insertion in Report, Reports, ReportService

--- src/main/java/REST/beans/Report.java
+++ src/main/java/REST/beans/Report.java
@@ -1,4 +1,22 @@
 package REST.beans;
 
 public class Report {
+
+    private int id;
+    private int pollution = 0;
+    private int ride = 0;
+    private int distance = 0;
+    private int battery = 100;
+
+    public Report() {}
+
+    public int getId() { return id; }
+
+    public int getPollution() { return pollution; }
+
+    public int getRide() { return ride; }
+
+    public int getDistance() { return distance; }
+
+    public int getBattery() { return battery; }
 }
--- src/main/java/REST/beans/Reports.java
+++ src/main/java/REST/beans/Reports.java
@@ -1,4 +1,36 @@
 package REST.beans;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
+import java.util.List;
+
+@XmlRootElement
+@XmlAccessorType (XmlAccessType.FIELD)
 public class Reports {
+
+    @XmlElement(name = "reports")
+    private List<Report> reportList;
+
+    private static Reports instance;
+
+    private Reports() { reportList = new ArrayList<Report>(); }
+
+    public synchronized static Reports getInstance() {
+
+        if (instance == null) {
+            instance = new Reports();
+        }
+
+        return instance;
+    }
+
+    public synchronized List<Report> getReportList() { return new ArrayList<>(reportList); }
+
+    public synchronized void add(Report report) {
+        reportList.add(report);
+    }
+
 }
