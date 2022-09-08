7a8ab5b05235c024d917710ce4c9e41e48a8e502
Mattia Fasoli <fasoxy@gmail.com>
2022-09-08 11:51:18 +0200

    Add Report Insertion in ReportService

--- src/main/java/REST/services/ReportService.java
+++ src/main/java/REST/services/ReportService.java
@@ -1,4 +1,26 @@
 package REST.services;
 
+import REST.beans.Report;
+import REST.beans.Reports;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+@Path("reports")
 public class ReportService {
+
+    @GET
+    @Produces("application/json")
+    public Response getReportList() {
+        return Response.ok(Reports.getInstance()).build();
+    }
+
+    @Path("add")
+    @POST
+    @Consumes("application/json")
+    @Produces("application/json")
+    public Response addReport(Report report) {
+        Reports.getInstance().add(report);
+        return Response.ok().build();
+    }
 }
