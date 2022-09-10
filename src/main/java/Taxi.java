7492751f1fc7139c31af0066f1574278ede7c4f7
Mattia Fasoli <fasoxy@gmail.com>
2022-09-10 11:02:44 +0200

    Add Rides in Taxi

--- src/main/java/Taxi.java
+++ src/main/java/Taxi.java
@@ -5,7 +5,10 @@ import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
-import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.*;
+
+import java.sql.Timestamp;
+import java.util.Scanner;
 
 public class Taxi {
 
@@ -27,6 +30,7 @@ public class Taxi {
         taxi.check();
         taxi.start();
         taxi.stats();
+        taxi.rides();
 
     }
 
@@ -101,6 +105,66 @@ public class Taxi {
             System.out.println(response);
         } catch (ClientHandlerException clientHandlerException) {
             clientHandlerException.printStackTrace();
+        }
+
+    }
+
+    public void rides() {
+
+        MqttClient client;
+        String broker = "tcp://localhost:1883";
+        String clientId = MqttClient.generateClientId();
+        String topic = "seta/smartcity/rides/district1";
+        int qos = 2;
+
+        try {
+
+            client = new MqttClient(broker, clientId);
+            MqttConnectOptions connectOptions = new MqttConnectOptions();
+            connectOptions.setCleanSession(true);
+
+            System.out.println(clientId + " Connecting Broker " + broker);
+            client.connect(connectOptions);
+            System.out.println("Connected");
+
+            client.setCallback(new MqttCallback() {
+                @Override
+                public void connectionLost(Throwable cause) {
+                    System.out.println(clientId + " Connection lost! cause:" + cause.getMessage());
+                }
+
+                @Override
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+
+                    String time = new Timestamp(System.currentTimeMillis()).toString();
+                    String receivedMessage = new String(message.getPayload());
+                    System.out.println(" Received a Message!" +
+                            "\n\tTime:    " + time +
+                            "\n\tTopic:   " + topic +
+                            "\n\tMessage: " + receivedMessage +
+                            "\n\tQoS:     " + message.getQos() + "\n");
+
+                    System.out.println("\n ***  Press a random key to exit *** \n");
+
+                }
+
+                @Override
+                public void deliveryComplete(IMqttDeliveryToken token) {
+
+                }
+            });
+
+            System.out.println("Subscribing ...");
+            client.subscribe(topic,qos);
+            System.out.println("Subscribed to topics : " + topic);
+
+            System.out.println("\n ***  Press a random key to exit *** \n");
+            Scanner command = new Scanner(System.in);
+            command.nextLine();
+            client.disconnect();
+
+        } catch (MqttException mqttException) {
+            mqttException.printStackTrace();
         }
 
     }
