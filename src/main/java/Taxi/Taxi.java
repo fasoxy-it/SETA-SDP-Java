1d30e160db5b76ed3edbcd634f61f419b266ef15
Mattia Fasoli <fasoxy@gmail.com>
2022-09-12 15:19:11 +0200

    Update Taxi

--- src/main/java/Taxi/Taxi.java
+++ src/main/java/Taxi/Taxi.java
@@ -1,15 +1,13 @@
-package Taxi;
+package taxi;
 
+import taxi.threads.ReportThread;
+import taxi.threads.RideThread;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientHandlerException;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
-import org.eclipse.paho.client.mqttv3.*;
-
-import java.sql.Timestamp;
-import java.util.Scanner;
 
 public class Taxi {
 
@@ -30,10 +28,16 @@ public class Taxi {
         Taxi taxi = new Taxi(id, ip, port);
         taxi.check();
         taxi.start();
-        taxi.stats();
-        taxi.rides();
+
+        ReportThread reportThread = new ReportThread(taxi);
+        RideThread rideThread = new RideThread(taxi);
+
+        reportThread.start();
+        rideThread.start();
 
     }
+
+    public int getId() { return id; }
 
     public void check() {
 
@@ -80,92 +84,6 @@ public class Taxi {
             System.out.println(response);
         } catch (ClientHandlerException clientHandlerException) {
             clientHandlerException.printStackTrace();
-        }
-
-    }
-
-    public void stats() {
-
-        Client client = Client.create();
-        JSONObject payload = new JSONObject();
-
-        try {
-
-            payload.put("taxi", id);
-            payload.put("battery", 100);
-
-        } catch (JSONException e) {
-            throw new RuntimeException(e);
-        }
-
-        WebResource webResource = client.resource("http://localhost:1337/reports/add");
-
-        try {
-            ClientResponse response = webResource.type("application/json").post(ClientResponse.class, payload);
-            response.getStatus();
-            System.out.println(response);
-        } catch (ClientHandlerException clientHandlerException) {
-            clientHandlerException.printStackTrace();
-        }
-
-    }
-
-    public void rides() {
-
-        MqttClient client;
-        String broker = "tcp://localhost:1883";
-        String clientId = MqttClient.generateClientId();
-        String topic = "seta/smartcity/rides/district1";
-        int qos = 2;
-
-        try {
-
-            client = new MqttClient(broker, clientId);
-            MqttConnectOptions connectOptions = new MqttConnectOptions();
-            connectOptions.setCleanSession(true);
-
-            System.out.println(clientId + " Connecting Broker " + broker);
-            client.connect(connectOptions);
-            System.out.println("Connected");
-
-            client.setCallback(new MqttCallback() {
-                @Override
-                public void connectionLost(Throwable cause) {
-                    System.out.println(clientId + " Connection lost! cause:" + cause.getMessage());
-                }
-
-                @Override
-                public void messageArrived(String topic, MqttMessage message) throws Exception {
-
-                    String time = new Timestamp(System.currentTimeMillis()).toString();
-                    String receivedMessage = new String(message.getPayload());
-                    System.out.println(" Received a Message!" +
-                            "\n\tTime:    " + time +
-                            "\n\tTopic:   " + topic +
-                            "\n\tMessage: " + receivedMessage +
-                            "\n\tQoS:     " + message.getQos() + "\n");
-
-                    System.out.println("\n ***  Press a random key to exit *** \n");
-
-                }
-
-                @Override
-                public void deliveryComplete(IMqttDeliveryToken token) {
-
-                }
-            });
-
-            System.out.println("Subscribing ...");
-            client.subscribe(topic,qos);
-            System.out.println("Subscribed to topics : " + topic);
-
-            System.out.println("\n ***  Press a random key to exit *** \n");
-            Scanner command = new Scanner(System.in);
-            command.nextLine();
-            client.disconnect();
-
-        } catch (MqttException mqttException) {
-            mqttException.printStackTrace();
         }
 
     }
