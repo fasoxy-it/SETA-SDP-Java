6abe8f71f6ad6eb339a0928f28ae4a4da04eb8ea
Mattia Fasoli <fasoxy@gmail.com>
2022-09-16 11:18:27 +0200

    Update GRPCTaxiServer

--- src/main/java/GRPC/GRPCTaxiServer.java
+++ src/main/java/GRPC/GRPCTaxiServer.java
@@ -1,4 +1,39 @@
 package GRPC;
 
-public class GRPCTaxiServer {
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import modules.Taxi;
+
+import java.io.IOException;
+
+public class GRPCTaxiServer extends Thread {
+
+    private Taxi taxi;
+
+    public GRPCTaxiServer(Taxi taxi) {
+        this.taxi = taxi;
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            Server server = ServerBuilder.forPort(taxi.getPort())
+                    .addService(new ManagerImpl(taxi))
+                    .build();
+
+            server.start();
+
+            System.out.println("Server started!");
+
+            server.awaitTermination();
+
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
 }
