d4e8080d3839ed8e79d30fd932c11a9898e103c0
Mattia Fasoli <fasoxy@gmail.com>
2022-09-16 11:18:02 +0200

    Add WelcomeThread in TaxiProcess

--- src/main/java/Taxi/TaxiProcess.java
+++ src/main/java/Taxi/TaxiProcess.java
@@ -1,8 +1,12 @@
 package taxi;
 
+import GRPC.GRPCTaxiServer;
 import modules.Taxi;
 import taxi.threads.ReportThread;
 import taxi.threads.RideThread;
+import taxi.threads.WelcomeThread;
+
+import java.util.Iterator;
 
 public class TaxiProcess {
 
@@ -18,11 +22,32 @@ public class TaxiProcess {
         taxi.check();
         taxi.start();
 
+        System.out.println(taxi);
+
         ReportThread reportThread = new ReportThread(taxi);
         RideThread rideThread = new RideThread(taxi);
+        GRPCTaxiServer grpcTaxiServer = new GRPCTaxiServer(taxi);
 
         reportThread.start();
         rideThread.start();
+        grpcTaxiServer.start();
+
+        if (taxi.getTaxiList().size() > 1) {
+
+            Iterator<Taxi> iterator = taxi.getTaxiList().iterator();
+
+            while (iterator.hasNext()) {
+
+                Taxi otherTaxi = iterator.next();
+
+                if (!(otherTaxi.getId() == taxi.getId())) {
+                    WelcomeThread welcomeThread = new WelcomeThread(taxi, otherTaxi);
+                    welcomeThread.run();
+                }
+            }
+        } else {
+            System.out.println("Alone Taxi!");
+        }
 
     }
 
