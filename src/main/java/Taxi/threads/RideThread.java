ab8833ce6b978f60420f8db140e5b9b3f918f579
Mattia Fasoli <fasoxy@gmail.com>
2022-09-28 23:32:54 +0200

    First implementation of a Ride

--- src/main/java/Taxi/threads/RideThread.java
+++ src/main/java/Taxi/threads/RideThread.java
@@ -1,95 +1,46 @@
 package taxi.threads;
 
 import MQTT.Ride;
-import com.google.gson.Gson;
+import javafx.geometry.Pos;
 import modules.Position;
 import modules.Taxi;
-import org.eclipse.paho.client.mqttv3.*;
-
-import java.sql.Timestamp;
-import java.util.Scanner;
 
 public class RideThread extends Thread {
 
-    private Taxi taxi;
+    Taxi taxi;
 
-    public RideThread(Taxi taxi) { this.taxi = taxi; }
+    Ride ride;
 
-    @Override
+    public RideThread(Taxi taxi, Ride ride) {
+        this.taxi = taxi;
+        this.ride = ride;
+    }
+
     public void run() {
 
-        MqttClient client;
-        String broker = "tcp://localhost:1883";
-        String clientId = MqttClient.generateClientId();
-        String topic = "seta/smartcity/rides/district";
-        String district = Position.getDistrict(taxi.getPosition());
-
-        topic = topic + district;
-
-        int qos = 2;
+        System.out.println("Doing the delivery of the order: " + ride.getId());
 
         try {
 
-            client = new MqttClient(broker, clientId);
-            MqttConnectOptions connectOptions = new MqttConnectOptions();
-            connectOptions.setCleanSession(true);
+            Thread.sleep(5000);
 
-            System.out.println("Connecting Broker " + broker);
-            client.connect(connectOptions);
-            System.out.println("Connected");
+            double distanceFromStartingPosition = Position.getDistance(taxi.getPosition(), ride.getStartingPosition());
+            System.out.println("Taxi arrived to the Starting Position!");
+            double distanceFromDestinationPosition = Position.getDistance(ride.getStartingPosition(), ride.getDestinationPosition());
+            System.out.println("Taxi arrived to the Destination Position!");
+            double distanceRide = distanceFromStartingPosition + distanceFromDestinationPosition;
+            System.out.println("Taxi finished the ride!");
 
-            Gson gson = new Gson();
+            int batteryConsumption = (int) Math.round(distanceRide);
 
-            client.setCallback(new MqttCallback() {
-                @Override
-                public void connectionLost(Throwable cause) {
-                    System.out.println("Connection lost! cause:" + cause.getMessage());
-                }
+            taxi.setPosition(ride.getDestinationPosition());
+            taxi.setBattery(taxi.getBattery() - batteryConsumption);
 
-                @Override
-                public void messageArrived(String topic, MqttMessage message) throws Exception {
+            System.out.println("New position: " + taxi.getPosition());
+            System.out.println("New battery level: " + taxi.getBattery());
 
-                    String time = new Timestamp(System.currentTimeMillis()).toString();
-                    String receivedMessage = new String(message.getPayload());
-                    System.out.println("Received a Message!" +
-                            "\n\tTime:    " + time +
-                            "\n\tTopic:   " + topic +
-                            "\n\tMessage: " + receivedMessage +
-                            "\n\tQoS:     " + message.getQos() + "\n");
-
-                    System.out.println("\n ***  Press a random key to exit *** \n");
-
-                    Ride ride = gson.fromJson(receivedMessage, Ride.class);
-                    taxi.addRideToList(ride);
-
-                    for (Taxi otherTaxi : taxi.getTaxiList()) {
-
-                        if (taxi.getId() != otherTaxi.getId()) {
-                            RideRequestThread rideRequestThread = new RideRequestThread(taxi, otherTaxi, ride);
-                            rideRequestThread.run();
-                        }
-
-                    }
-
-                }
-
-                @Override
-                public void deliveryComplete(IMqttDeliveryToken token) {
-
-                }
-            });
-
-            System.out.println("Subscribing ...");
-            client.subscribe(topic,qos);
-            System.out.println("Subscribed to topics : " + topic);
-
-            System.out.println("\n ***  Press a random key to exit *** \n");
-            Scanner command = new Scanner(System.in);
-            command.nextLine();
-            client.disconnect();
-
-        } catch (MqttException mqttException) {
-            mqttException.printStackTrace();
+        } catch (InterruptedException interruptedException) {
+            interruptedException.printStackTrace();
         }
 
     }
